3a37c22ffe91f2a71827dd90a6dea4eb6075fe68
Alexey Gorshkov <agorshkov23@mail.ru>
2017-02-17 10:21:07 +0300

    Release 1.0.0-RELEASE

---
+++ src/main/java/ml/alegor/udpduplicator/Application.java
@@ -0,0 +1,46 @@
+package ml.alegor.udpduplicator;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Application {
+    public static void main(String[] args) throws Exception {
+        InputStream inputStream = new FileInputStream("config.properties");
+        if (inputStream == null) {
+            System.err.println("Configuration file not found!");
+            System.exit(1);
+        }
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        String listener = properties.getProperty("listener");
+        String destinations = properties.getProperty("destinations");
+        SocketAddress inSocketAddress = parseSocketAddress(listener);
+        Set<SocketAddress> outSocketAddressSet = Arrays.stream(destinations.split(",")).map(s -> parseSocketAddress(s)).collect(Collectors.toSet());
+
+        DatagramSocket inSocket = new DatagramSocket(inSocketAddress);
+        DatagramSocket outSocket = new DatagramSocket();
+        byte[] buffer = new byte[4 * 1024];
+        while (true) {
+            DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+            inSocket.receive(inPacket);
+            for (SocketAddress outSocketAddress : outSocketAddressSet) {
+                outSocket.send(new DatagramPacket(buffer, inPacket.getLength(), outSocketAddress));
+            }
+        }
+    }
+
+    private static InetSocketAddress parseSocketAddress(String socketAddress) {
+        String[] split = socketAddress.split(":");
+        String host = split[0];
+        int port = Integer.parseInt(split[1]);
+        return new InetSocketAddress(host, port);
+    }
+}
